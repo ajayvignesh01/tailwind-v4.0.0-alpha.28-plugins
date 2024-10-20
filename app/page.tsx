d8e175a4747241ec6e62afcbf2ebbde8154a9617
Ajay <ajayvignesh01@gmail.com>
2024-10-20 15:41:10 -0400

    fix: prettier tailwind entry point & remove v3 tailwind config

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,15 @@
-import { Button } from "@/components/ui/button";
+import { Button } from '@/components/ui/button'
 
 export default function Home() {
-	return (
-		<div className="animate-in fade-in zoom-in flex h-screen w-screen flex-col items-center justify-center gap-4">
-			<div className="flex flex-col gap-4">
-				<Button variant="default">Default</Button>
-				<Button variant="secondary">Secondary</Button>
-				<Button variant="outline">Outline</Button>
-				<Button variant="destructive">Destructive</Button>
-				<Button variant="ghost">Ghost</Button>
-			</div>
-		</div>
-	);
+  return (
+    <div className='flex h-screen w-screen flex-col items-center justify-center gap-4 animate-in fade-in zoom-in'>
+      <div className='flex flex-col gap-4'>
+        <Button variant='default'>Default</Button>
+        <Button variant='secondary'>Secondary</Button>
+        <Button variant='outline'>Outline</Button>
+        <Button variant='destructive'>Destructive</Button>
+        <Button variant='ghost'>Ghost</Button>
+      </div>
+    </div>
+  )
 }
